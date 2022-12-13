a472b0a0e1fbb4c61dc889cbe300f9dac753ed72
Renato Fernandes <renatodepsf@gmail.com>
2022-12-13 16:11:34 -0300

    Primeiro exercício curso POO - guanabara

---
+++ src/Caneta.java
@@ -0,0 +1,40 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar() {
+        if (tampada == true) {
+            System.out.println("Caneta tampada.!");
+        }
+        else {
+            System.out.println("Rabiscando!");
+        }
+    }
+
+    void tampar() {
+        if (tampada == true) {
+            System.out.println("A caneta já está tampada.");
+        }
+        else {
+            System.out.println("Tampando!");
+            this.tampada = true;
+        }
+    }
+
+    void destampar() {
+        if (tampada == true) {
+            System.out.println("Destampando!");
+            this.tampada = false;
+        } else {
+            System.out.println("A caneta já destampada.");
+        }
+    }
+
+    void status() {
+        System.out.println();
+        System.out.printf("Cor: %s\nPonta: %.2f\nCarga: %d\nTampada: %b\nModelo: %s", this.cor, this.ponta, this.carga, this.tampada, this.modelo);
+    }
+}
